bbe0693187aad527cce79b996d66859f0e64a3a6
akash-1316 <freefireakash73@gmail.com>
2025-07-04 20:21:19 +0530

    Update App.jsx

--- admin/src/App.jsx
+++ admin/src/App.jsx
@@ -10,7 +10,7 @@ import "react-toastify/dist/ReactToastify.css";
 import Login from "./components/Login/Login";
 
 const App = () => {
-  const url = "http://localhost:5000";
+  const url = "https://gourmet-go-1.onrender.com";
   return (
     <div>
       <ToastContainer />
