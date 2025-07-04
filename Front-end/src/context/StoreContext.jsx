fba5eca64b9bb1cacea67af52106570b982a680b
akash-1316 <freefireakash73@gmail.com>
2025-07-04 20:20:02 +0530

    Update StoreContext.jsx

--- Front-end/src/context/StoreContext.jsx
+++ Front-end/src/context/StoreContext.jsx
@@ -6,7 +6,7 @@ export const StoreContext = createContext(null);
 
 const StoreContextProvider = (props) => {
   const [cartItems, setCartItems] = useState({});
-  const url = "http://localhost:5000";
+  const url = "https://gourmet-go-1.onrender.com";
   const [token, setToken] = useState("");
   const [food_list, setFoodList] = useState([]);
 
